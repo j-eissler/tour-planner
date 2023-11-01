85942a39676bf958151b50f83e229d377ab84c93
j-eissler <57557568+j-eissler@users.noreply.github.com>
2023-11-01 22:24:53 +0100

    Add map screen centered on city of Aachen

---
+++ lib/map_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({super.key});
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return FlutterMap(
+        options: const MapOptions(initialCenter: LatLng(50.775555, 6.083611)),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            userAgentPackageName: 'com.example.app',
+          ),
+        ]);
+  }
+}
