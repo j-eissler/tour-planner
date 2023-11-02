6138c979d80990b76e49e63dc9ab8b0fc247bcb0
j-eissler <57557568+j-eissler@users.noreply.github.com>
2023-11-02 19:10:47 +0100

    Add textfields to waypoint form and add data to database on submit

--- lib/waypoint_form_screen.dart
+++ lib/waypoint_form_screen.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:tour_planner/database.dart';
+import 'package:tour_planner/waypoint_model.dart';
 
-class WaypointFormScreen extends StatelessWidget {
+class WaypointFormScreen extends StatefulWidget {
   const WaypointFormScreen({super.key});
+
+  @override
+  State<WaypointFormScreen> createState() => _WaypointFormScreenState();
+}
+
+class _WaypointFormScreenState extends State<WaypointFormScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String _address = '';
+  String _city = '';
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +20,44 @@ class WaypointFormScreen extends StatelessWidget {
       appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: const Text("Enter waypoint information")),
-      body: const Center(child: Text("Enter data here")),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            TextFormField(
+                decoration: const InputDecoration(
+                    icon: Icon(Icons.location_on), labelText: 'Address'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'This field cannot be empty';
+                  }
+                  _address = value;
+                  return null;
+                }),
+            TextFormField(
+                decoration:
+                    const InputDecoration(icon: Icon(null), labelText: 'City'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'This field cannot be empty';
+                  }
+                  _city = value;
+                  return null;
+                }),
+            ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Processing data')),
+                    );
+                  }
+                  final db = TourPlannerDatabase();
+                  db.addWaypoint(Waypoint(address: _address, city: _city));
+                },
+                child: const Text('Submit')),
+          ],
+        ),
+      ),
     );
   }
 }
