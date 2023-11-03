d2928bdeb9debe24059d7e6e68b54711afa0b818
j-eissler <57557568+j-eissler@users.noreply.github.com>
2023-11-03 01:07:58 +0100

    Place markers for waypoints on the map

--- lib/map_screen.dart
+++ lib/map_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:tour_planner/database.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -10,6 +11,14 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  List<Marker> _markers = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadMarkers();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,7 +37,11 @@ class _MapScreenState extends State<MapScreen> {
             ListTile(
               leading: const Icon(Icons.view_list),
               title: const Text("Waypoints"),
-              onTap: () => Navigator.pushNamed(context, '/waypoints_list'),
+              onTap: () async {
+                // Wait until returned from next screen
+                await Navigator.pushNamed(context, '/waypoints_list');
+                loadMarkers();
+              },
             ),
             // TODO: Add AboutListTile
           ],
@@ -46,7 +59,38 @@ class _MapScreenState extends State<MapScreen> {
               urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
               userAgentPackageName: 'com.example.app',
             ),
+            MarkerLayer(
+              markers: _markers,
+              alignment: Alignment.topCenter,
+            ),
           ]),
     );
+  }
+
+  void loadMarkers() async {
+    final db = TourPlannerDatabase();
+    var waypoints = await db.getAllWaypoints();
+
+    List<Marker> markers = [];
+    for (var wp in waypoints) {
+      if (wp.lat == null || wp.long == null) continue;
+
+      Marker m = Marker(
+        point: LatLng(wp.lat!, wp.long!),
+        width: 40,
+        height: 40,
+        child: const FittedBox(
+          child: Icon(
+            Icons.location_on,
+            color: Colors.red,
+          ),
+        ),
+      );
+      markers.add(m);
+    }
+
+    setState(() {
+      _markers = markers;
+    });
   }
 }
